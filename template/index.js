e284a05a5df6f2f621101cd8cf2deb696a8fb408
Adam Kiss <iam@adamkiss.com>
2018-01-29 15:44:38 +0100

    More customising

--- template/index.js
+++ template/index.js
@@ -1,30 +1,12 @@
-const foo = require('foo');
-
 /**
  * Documentation: Writing Plugins
  * @see https://github.com/lukeed/taskr#plugin
  * @see https://github.com/lukeed/taskr#external-plugins
  */
 module.exports = function (task, utils) {
-	// promisify before running else repeats per execution
-	const render = utils.promisify(foo.bar);
-
-	// Option #1
 	task.plugin('<%= slugName %>', {/* every:true, files:true */}, function * (file, opts) {
 		console.log('a single file object', file); //=> { base, dir, data }
 		console.log('user-provided config', opts); //=> null || {}
 		yield render(opts);
 	});
-
-	// Option #2
-	/*
-		task.plugin({
-			name: '<%= slugName %>',
-			every: true,
-			files: true,
-			*func(file, opts) {
-				// ...same
-			}
-		});
-	 */
 };
